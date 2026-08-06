075695e921832a56399921e0c9b91e70cfd85d23
r2eyy <r2eyyr2@gmail.com>
2026-08-06 21:36:30 +0200

    Replace max_penetration_tick with big::max_penetration_tick

--- src/backend/backend.cpp
+++ src/backend/backend.cpp
@@ -91,8 +91,7 @@ namespace big
 			looped::weapons_c4_limit();
 			looped::weapons_do_persist_weapons();
 			looped::weapons_do_weapon_hotkeys();
-
-			max_penetration_tick();
+            big::max_penetration_tick();
 
 			script::get_current()->yield();
 		}
